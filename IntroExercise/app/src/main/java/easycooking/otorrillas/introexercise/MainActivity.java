36c31fcaaffaa00697c1f25b324d4417d151c59a
Oriol Torrillas <otorrillas@gmail.com>
2016-02-01 18:39:02 +0100

    Added Sheep library

--- IntroExercise/app/src/main/java/easycooking/otorrillas/introexercise/MainActivity.java
+++ IntroExercise/app/src/main/java/easycooking/otorrillas/introexercise/MainActivity.java
@@ -2,13 +2,22 @@ package easycooking.otorrillas.introexercise;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import 
+import sheep.game.Game;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Create the game.
+        Game game = new Game(this, null);
+
+        // Push the main state.
+        game.pushState(new TitleScreen());
+
+        // View the game.
+        setContentView(game);
     }
 }
---
+++ IntroExercise/app/src/main/java/easycooking/otorrillas/introexercise/TitleScreen.java
@@ -0,0 +1,10 @@
+package easycooking.otorrillas.introexercise;
+
+import sheep.game.State;
+
+/**
+ * Created by otorr on 01/02/2016.
+ */
+public class TitleScreen extends State {
+
+}
